ddde3536d74c7ca1e28665c966d40c12fa434dd4
pingoDeLingo <robin.noormets@voco.ee>
2024-02-15 13:30:47 +0200

    WiP google login

--- App.js
+++ App.js
@@ -4,16 +4,18 @@ import Splash from "./src/screens/auth/Splash";
 import Signup from "./src/screens/auth/Signup";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
 
-const IOS_CLIENT_ID = "889267781491-q61okfnjmecstlrgha4rs072iro0c6nv.apps.googleusercontent.com";
-const REVERSED_CLIENT_ID = "com.googleusercontent.apps.889267781491-q61okfnjmecstlrgha4rs072iro0c6nv";
+const IOS_CLIENT_ID = "889267781491-ui4bsatr64dgibavabdfjkql9ositd0c.apps.googleusercontent.com";
+const WEB_CLIENT_ID = "889267781491-q61okfnjmecstlrgha4rs072iro0c6nv.apps.googleusercontent.com";
+const REVERSED_CLIENT_ID = "com.googleusercontent.apps.889267781491-ui4bsatr64dgibavabdfjkql9ositd0c";
 
 const App = () => {
   useEffect(() => {
-    GoogleSignin.configure({
-      webClientId: IOS_CLIENT_ID,
-      offlineAccess: true,
-      iosClientId: IOS_CLIENT_ID,
-    });
+      GoogleSignin.configure({
+          scopes: ['https://www.googleapis.com/auth/drive.readonly'],
+          webClientId: WEB_CLIENT_ID,
+          offlineAccess: true,
+          iosClientId: IOS_CLIENT_ID,
+      })
   }, [])
   return (
     <SafeAreaView>
--- src/components/GoogleLogin/index.js
+++ src/components/GoogleLogin/index.js
@@ -1,36 +1,34 @@
 import React from "react";
-import {Image, TouchableOpacity } from "react-native";
-import { styles } from "./styles";
+import { Image, TouchableOpacity } from "react-native";
+import {styles} from './styles'
 
-import { GoogleSignin, statusCodes } from "@react-native-google-signin/google-signin";
-
+import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
 
 const GoogleLogin = () => {
     const signIn = async () => {
         try {
-            await GoogleSignin.hasPlayServices();
-            const userInfo = await GoogleSignin.signIn();
-            console.log(userInfo);
+          await GoogleSignin.hasPlayServices();
+          const userInfo = await GoogleSignin.signIn();
+          // this.setState({ userInfo });
+          console.log('userInfo => ', userInfo)
         } catch (error) {
-            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-                console.log("user cancelled the login flow");
-            } else if (error.code === statusCodes.IN_PROGRESS) {
-                console.log("operation (e.g. sign in) is in progress already");
-            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-                console.log("play services not available or outdated");
-            } else {
-                console.log("some other error happened");
-            }
+          if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+            // user cancelled the login flow
+          } else if (error.code === statusCodes.IN_PROGRESS) {
+            // operation (e.g. sign in) is in progress already
+          } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+            // play services not available or outdated
+          } else {
+            // some other error happened
+          }
         }
-    }
-    
+      }
 
-    return (
-        <TouchableOpacity activeOpacity={0.6} style={styles.container}>
-            <Image style={styles.image} source={require("../../assets/google.png")} />
+    return(
+        <TouchableOpacity activeOpacity={0.6} onPress={signIn} style={styles.container}>
+            <Image style={styles.image} source={require('../../assets/google.png')}/>
         </TouchableOpacity>
     )
 }
 
-export default React.memo(GoogleLogin);
-
+export default React.memo(GoogleLogin)
